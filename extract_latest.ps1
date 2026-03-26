b254d978ffdc08ab9408d5588bc36a6df1a4f64d
Carl Wang <cogear@qq.com>
2026-03-27 02:35:45 +0800

    extract script

---
+++ extract_latest.ps1
@@ -0,0 +1,7 @@
+# ########## 用户配置 ##########
+# 设置你的Dota 2 pak01_dir.vpk目标路径
+$SourceVPK = "E:\SteamLibrary\steamapps\common\dota 2 beta\game\dota\pak01_dir.vpk"
+# ############################
+
+
+vpkeditcli --extract "scripts/npc/" -o "./pak01_dir/scripts/npc/" $SourceVPK
